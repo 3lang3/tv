971450107a69045493ae91a9068ad59dd7231f55
浪浪浪 <675483520@qq.com>
2017-06-05 20:43:27 +0800

    add recommend comp

---
+++ src/components/Recommend/Recommend.jsx
@@ -0,0 +1,18 @@
+import React from './react';
+import { connect } from 'react-redux';
+
+import styles from './Recommmend.css';
+
+class Recommmend extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    rend() {
+
+        return (
+            <div>Recommmend content</div>
+        )
+    }
+}
