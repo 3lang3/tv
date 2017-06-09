146c16b407ec5e12f4298e04aa1a8aef510a7eb5
浪浪浪 <675483520@qq.com>
2017-06-09 22:50:43 +0800

    update changlog

--- src/components/Changelog/Changelog.jsx
+++ src/components/Changelog/Changelog.jsx
@@ -46,18 +46,17 @@ class ChangeLog extends React.Component {
                 autoScrollBodyContent={true}
             >
                 <section className={styles.changeLogSec}>
-                    <h4>版本更新<span>version 1.2.0</span></h4>
+                    <h4>版本更新<span>version 2.0.0</span></h4>
                     <img src={require('../../../assets/changelog.jpg')}alt="" />
                     <h2>
                         更新内容
                     </h2>
                     <ul>
-                        <li>添加Favorite关注列表</li>
-                        <li>更改主舞台空闲状态UI(星系ui)</li>
-                        <li>优化源数据</li>
-                        <li>优化注册登录UI</li>
+                        <li><strong>重要提示: Douyu直播播放器会造成某些浏览器直接卡死</strong></li>
+                        <li>更改首页布局</li>
+                        <li>重构主舞台空闲状态UI, 现改为hot和banner栏目</li>
+                        <li>Chat模块从首页移除</li>
                         <li>添加Github开源地址,期待你的加入 <strong><a href="https://github.com/EthanOrange/tv" target="_blank">View Github</a></strong></li>
-                        <li>添加赞助功能, 希望有能力的伙伴能和网站一同前行</li>
                         <li>依然一大堆BUG和ISSUE <strong>#NeedYOU</strong></li>
                     </ul>
                     <div style={{
